02076dc8dd6f2588bbca40a1bb3d8ebbb6b97917
Aushzlten12 <josepachas1604@gmail.com>
2023-12-17 21:05:18 -0500

    Acabando pruebas para index y show

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,4 +4,18 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test 'render a list of products' do 
+    get products_path 
+    assert_response :success
+    assert_select '.product', 2
+  end
+
+  test 'render a detailed product page' do
+    get product_path(products(:ps4))
+    assert_response :success
+    assert_select '.title', 'PS4 fat'
+    assert_select '.description', 'Buen Estado'
+    assert_select '.price', '1600$'
+  end
+
 end
