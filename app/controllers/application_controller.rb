bd208d447d12d6c4dd0b43233d64fe8ea0fb8b03
Aushzlten12 <josepachas1604@gmail.com>
2023-12-19 17:36:38 -0500

    Agregando el controlador categorias

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-	around_action :switch_locale
+  around_action :switch_locale
 
-	def switch_locale(&action)
-		I18n.with_locale(locale_from_header, &action)
-	end
+  def switch_locale(&action)
+    I18n.with_locale(locale_from_header || I18n.default_locale.to_s, &action)
+  end
 
-	private
+  private
 
-	def locale_from_header
-		request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-	end
+  def locale_from_header
+    request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first
+  end
 end
