4cfef3157c8d6396142a0d1d890d605b6f86efe3
Aushzlten12 <josepachas1604@gmail.com>
2023-12-21 16:46:24 -0500

    Probandolo en neovim

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,48 +1,50 @@
 class ProductsController < ApplicationController
-	def index
-		@products = Product.all.with_attached_photo.order(created_at: :desc)
-		@products.order(:id)
-	end
+  def index
+    @categories = Category.all
+    @products = Product.all.with_attached_photo.order(created_at: :desc)
+    @products.order(:id)
+  end
 
-	def show
-		product
-	end
-	def new
-		@product = Product.new
-	end
+  def show
+    product
+  end
 
-	def create
-		@product = Product.new(product_params) 
-		if @product.save
-			redirect_to products_path, notice: t('.created')
-		else
-			render :new, status: :unprocessable_entity
-		end
-	end
+  def new
+    @product = Product.new
+  end
 
-	def edit
-		product
-	end
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to(products_path, notice: t(".created"))
+    else
+      render(:new, status: :unprocessable_entity)
+    end
+  end
 
-	def update
-		if product.update(product_params)
-			redirect_to products_path, notice: t('.updated')
-		else
-			render :edit, status: :unprocessable_entity
-		end
-	end
+  def edit
+    product
+  end
 
-	def destroy
-		product.destroy
-		redirect_to products_path, notice: t('.destroyed') , status: :see_other
-	end 
+  def update
+    if product.update(product_params)
+      redirect_to(products_path, notice: t(".updated"))
+    else
+      render(:edit, status: :unprocessable_entity)
+    end
+  end
 
-	private
-	def product_params
-		params.require(:product).permit(:title, :description, :price, :photo, :category_id)
-	end
+  def destroy
+    product.destroy
+    redirect_to(products_path, notice: t(".destroyed"), status: :see_other)
+  end
 
-	def product
-		@product = Product.find(params[:id])
-	end
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :photo, :category_id)
+  end
+
+  def product
+    @product = Product.find(params[:id])
+  end
 end
